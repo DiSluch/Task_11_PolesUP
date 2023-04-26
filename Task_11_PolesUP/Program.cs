95aee32690d2972734f55b3279dec9a2142143aa
Дима Случак <pripru916@gmail.com>
2023-04-27 00:39:43 +0300

    Добавьте файлы проекта.

---
+++ Task_11_PolesUP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11_PolesUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите элементы массива:");
+
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < (n-1) - i; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int min = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = min;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{nums[i]}\t");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
